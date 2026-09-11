e72c8208ba0d5480b996c3ad4bec62f09a0dd55c
Danzel <2605648@sit.singaporetech.edu.sg>
2026-09-11 15:03:36 +0800

    Users-input

--- OneDrive/Desktop/Programming Fundamentals with DevOps (INF 1103)/Week 2/auditor.py
+++ OneDrive/Desktop/Programming Fundamentals with DevOps (INF 1103)/Week 2/auditor.py
@@ -1,3 +1,16 @@
 ##Set inventory to '0'
 inventory=0
 
+##User-input
+user_input= input("Enter a stock quantity: ")
+
+
+while user_input != "quit":
+    user_input= input("Enter a stock quantity: ")
+
+    if user_input == "quit":
+        print("Exiting the program...")
+        
+    else:
+        stock_input = int(user_input)
+        inventory = inventory + stock_input

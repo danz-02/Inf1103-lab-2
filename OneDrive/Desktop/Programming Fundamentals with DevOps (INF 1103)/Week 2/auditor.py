dff97eeea8534421abfa85d1b5f69b739e9f18e4
Danzel <2605648@sit.singaporetech.edu.sg>
2026-09-12 00:09:50 +0800

    Valid-input

--- OneDrive/Desktop/Programming Fundamentals with DevOps (INF 1103)/Week 2/auditor.py
+++ OneDrive/Desktop/Programming Fundamentals with DevOps (INF 1103)/Week 2/auditor.py
@@ -1,16 +1,20 @@
 ##Set inventory to '0'
 inventory=0
+max_inventory=500
+failed_entries=0
 
-##User-input
-user_input= input("Enter a stock quantity: ")
 
 
-while user_input != "quit":
+while True:
+    ##User-input
     user_input= input("Enter a stock quantity: ")
 
     if user_input == "quit":
         print("Exiting the program...")
-        
-    else:
+        break
+    ##Check for valid-input
+    elif user_input.isdigit():
+    
         stock_input = int(user_input)
-        inventory = inventory + stock_input
+
+        
